822fb383e337ae7c6c2cf2aec8cf4cd813791eb9
Kelsey Judson <kelseyjudson@gmail.com>
2015-04-28 19:56:17 +1200

    Fix Token middleware (somehow, I don't know damn it).

--- lib/lucid_cm/middleware/token.rb
+++ lib/lucid_cm/middleware/token.rb
@@ -3,42 +3,36 @@ require 'json'
 module LucidCM::Middleware
   class Token < Base
 
+    TOKEN_CODES = [ 50, *120..122 ]
+
     def call( env )
-      app.call( env ).on_complete do |env|
-        code = _code( env )
-
-        break unless _unauthorized?( env )
-        break unless _token_codes.include?( code )
-
-        begin
-          raise LucidCM::Exceptions::InvalidToken.new( code )
-        rescue => e
-          log_error( e.message )
-
-          raise e
-        end
-      end
+      app.call( env ).on_complete &method( :_response_handler )
     end
 
     private
 
-    def _code( env )
-      b = _body( env )
-
-      b['Code'] if b.kind_of?( Hash )
-    end
-
-    def _body( env )
-      JSON.parse( env[:body] )
+    def _data( res )
+      JSON.parse( res[:body] )
     rescue JSON::ParserError
     end
 
-    def _unauthorized?( env )
-      env[:status] == 401
+    def _code( data )
+      data['Code'] if data.kind_of?( Hash )
     end
 
-    def _token_codes
-      [ 50, 120, 121, 122 ]
+    def _response_handler( res )
+      data = _data res
+      code = _code data
+
+      if res[:status] == 401 && TOKEN_CODES.include?( code )
+        _raise_for( code )
+      end
+    end
+
+    def _raise_for( code )
+      raise LucidCM::Exceptions::InvalidToken.new( code ).tap do |e|
+        log_error( e.message )
+      end
     end
 
   end
